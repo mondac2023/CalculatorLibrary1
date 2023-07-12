5a2a51bcb424fd11d26144273d83b860df1aedf8
mondac2023 <chiranjib.mondal@tii.ae>
2023-07-12 15:34:37 +0400

    remove lines

--- test_Calculator.py
+++ test_Calculator.py
@@ -3,8 +3,7 @@ import Calculator
 
 class Test_Calculator:
     def test_addition(self):
-        assert Calculator.add(2,2) == 4
-
+        assert Calculator.add(2, 2) == 4
 
     def test_subtraction(self):
-        assert Calculator.subtract(4,3) == 1
+        assert Calculator.subtract(4, 3) == 1
